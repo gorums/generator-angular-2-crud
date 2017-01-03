ae4eaefbf606b03427c95a831b3dce866ae8d9bf
gorums <acksecurity@hotmail.com>
2017-01-03 02:16:05 -0500

    Fix the model

--- generators/app/templates/src/app/models/entity.ts
+++ generators/app/templates/src/app/models/entity.ts
@@ -1,5 +1,5 @@
 export interface <%= entity.capitalize %>Model {
   <% Object.keys(entity.entity).forEach(function(field) {%>
-    <%= field %><% if(!entity.entity[field].require) {%>?<%}%>: <%= entity.entity[field].type || entity.entity[field] %>;
+    <%= field %><% if(!entity.entity[field].key && !entity.entity[field].require) {%>?<%}%>: <%= entity.entity[field].type || entity.entity[field] %>;
   <% })%>
 }
