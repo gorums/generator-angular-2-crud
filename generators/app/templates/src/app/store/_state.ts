94b2ffc6ef2561d6dcc7f1c921d4ea625fa0a12a
gorums <acksecurity@hotmail.com>
2017-01-05 16:48:55 -0500

    Refactor the generation code

--- generators/app/templates/src/app/store/_state.ts
+++ generators/app/templates/src/app/store/_state.ts
@@ -1,14 +1,17 @@
 import {
-  <% entities.forEach(function (entity) {%><%= entity.capitalize %>Model,
-  <% })%>
+  <% entities.forEach(function (entity) { -%>
+  <%= entity.capitalize %>Model,
+  <% }) -%>
 }  from '../models';
 
 export interface State {
-  <% entities.forEach(function (entity) {%><%= entity.pluralizeUncapitalize %>: Array<<%= entity.capitalize %>Model>
-  <% })%>
+  <% entities.forEach(function (entity) { -%>
+  <%= entity.pluralizeUncapitalize %>: Array<<%= entity.capitalize %>Model>
+  <% }) -%>
 }
 
 export const defaultState = {
-  <% entities.forEach(function (entity) {%><%= entity.pluralizeUncapitalize %>: [],
-  <% })%>
+  <% entities.forEach(function (entity) { -%>
+  <%= entity.pluralizeUncapitalize %>: [],
+  <% }) -%>
 }
