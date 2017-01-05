6231fa26560a3ced05e348fb8f0459ba6567c515
gorums <acksecurity@hotmail.com>
2017-01-05 16:45:15 -0500

    Refactor the generation code

--- generators/app/templates/src/app/models/_index.ts
+++ generators/app/templates/src/app/models/_index.ts
@@ -1,2 +1,3 @@
-<% entities.forEach(function (entity) {%>export { <%= entity.capitalize %>Model } from './<%= entity.uncapitalize %>';
-<% })%>
+<% entities.forEach(function (entity) { -%>
+export { <%= entity.capitalize %>Model } from './<%= entity.uncapitalize %>';
+<% }) -%>
